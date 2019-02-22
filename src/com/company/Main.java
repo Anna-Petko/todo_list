b4a913a01c70081a626033615e8f58dae8d2b9ef
Anna-Petko <annapetko@icloud.com>
2019-02-22 15:47:34 +0100

    Adding method for task deleting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,9 @@ public class Main {
       Task first = new Task("finish smth", "12 march", "noName");
       TasksList task1 = new TasksList();
       task1.addTasksToList(first);
-
+      TasksList collection = new TasksList();
+      collection.addTasksToList(first);
+        System.out.println(collection);
+        System.out.println( collection.getTask(0));
     }
 }
